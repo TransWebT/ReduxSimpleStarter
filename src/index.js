56d7d0d310671183d61304f11a5c35baf43293a8
Terry Tompkins <tompkins@transwebtools.com>
2017-07-12 20:08:12 -0400

    code as of section 1, lesson 15

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBMpQ7Gp_WJUN4NfbjEntbquA3Z2OFNIXo';
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
